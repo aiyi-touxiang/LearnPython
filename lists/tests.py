87a0f85e64712b1038c128260dbdf59160c003e9
aiyi-touxiang <598307691@qq.com>
2016-01-12 17:16:05 +0800

    首次映射url路径到代码视图的单元测试

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
 # 故意编写一个会失败的愚蠢的测试
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePage(TestCase):
+    def test_root_url_resolves_to_home_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
