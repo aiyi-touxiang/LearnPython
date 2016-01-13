b061038094046eccd93dea83762283a645f84277
aiyi-touxiang <598307691@qq.com>
2016-01-13 15:02:36 +0800

    现在Functional test检查我们能否写入一条to-do

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 # 测试组织成类的形式,继承自 unittest.TestCase。
+
+
 class NewVisitorTest(unittest.TestCase):
 
     # setUp 和 tearDown 是特殊的方法,分别在各个测试方法之前和之后运行。
@@ -28,18 +31,34 @@ class NewVisitorTest(unittest.TestCase):
         # 如 assertEqual、assertTrue 和 assertFalse 等。更多断言辅助函 数参见 unittest 的文档,
         # 地址是 http://docs.python.org/3/library/unittest.html。
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # 她在一个文本框中输入了“Buy peacock feathers(”购买孔雀羽毛)
         # 伊迪丝的爱好是使用假蝇做饵钓鱼
+        inputbox.send_keys('Buy peacock feathers')
 
         # 她按回车键后,页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list-table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # 页面中又显示了一个文本框,可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly(”使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
+        self.fail('Finish the test!')
 
         # 页面再次更新,她的清单中显示了这两个待办事项
         # 伊迪丝想知道这个网站是否会记住她的清单
@@ -52,7 +71,7 @@ class NewVisitorTest(unittest.TestCase):
         # 她很满意,去睡觉了
 
         # 不管怎样,self.fail 都会失败,生成指定的错误消息。我使用这个方法提醒测试结束了。
-        self.fail('Finish the test!')
+
 
 # 最后是 if __name__ == '__main__' 分句(如果你之前没见过这种用法,我告诉你,
 # Python 脚本使用这个语句检查自己是否在命令行中运行,而不是在其他脚本中导入)。
